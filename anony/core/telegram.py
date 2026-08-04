540c8f6e505b444add68e5dbec3ba9503b6bdb00
core-999 <devcores999@gmail.com>
2026-08-04 21:42:45 +0630

    Dev999
    Signed-off-by: core-999 <devcores999@gmail.com>

--- anony/core/telegram.py
+++ anony/core/telegram.py
@@ -58,7 +58,7 @@ class Telegram:
             await sent.edit_text(sent.lang["play_duration_limit"].format(config.DURATION_LIMIT // 60))
             return await sent.stop_propagation()
 
-        if file_size > 200 * 1024 * 1024:
+        if file_size > 4000 * 1024 * 1024:
             await sent.edit_text(sent.lang["dl_limit"])
             return await sent.stop_propagation()
 
